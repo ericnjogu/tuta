8bea0f3e0a8aa6d14108724d99caded4230f8516
Eric Mugo <kunadawa@gmail.com>
2013-12-24 21:09:14 +0300

    dynamically generating secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,18 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tuta::Application.config.secret_key_base = '50014e6fc26c7841295d754a9b96117f038eedc12a05dff40b4d37879f016e633ef69f074bcb649976b1b93f296df254497a415a68aedd5316df8fe7f6df0c88'
+#Tuta::Application.config.secret_key_base = '50014e6fc26c7841295d754a9b96117f038eedc12a05dff40b4d37879f016e633ef69f074bcb649976b1b93f296df254497a415a68aedd5316df8fe7f6df0c88'
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		File.read(token_file).chomp
+	else
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+Tuta::Application.config.secret_key_base = secure_token
