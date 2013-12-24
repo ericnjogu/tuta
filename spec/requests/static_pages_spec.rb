b9e2bdc791dd441deb0473ed455384693832b7dc
Eric Mugo <kunadawa@gmail.com>
2013-12-24 23:31:40 +0300

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Home')
+    end
+    it " title should contain 'Tuta - Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Tuta - Home')
+    end
+  end
+   describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+     it " title should contain 'Tuta - Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Tuta - Help')
+    end
+  end
+   describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it " title should contain 'Tuta - About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Tuta - About Us')
     end
   end
 end
